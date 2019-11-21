6928e443c20b4d7e260d723e9b1604514f0b5b0b
jeff-chan-sf-sei-05 <emailjeffchan@gmail.com>
2019-11-21 10:59:53 -0800

    register is working with the class backend

--- src/config/routes.js
+++ src/config/routes.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import { Switch, Route } from 'react-router-dom';
-import Home from "../components/Home/Home"
+import Home from "../components/Home/Home";
+import Register from '../components/Auth/Register';
 
 export default () => 
 (
   <Switch>
     <Route exact path="/" component={Home} />
+    <Route path='/register' component={Register} />
   </Switch>
 )
