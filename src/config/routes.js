1d93d79ed3b5b0fc327295a67a794824c910d517
jacklok <jack22hk@gmail.com>
2019-11-21 12:07:19 -0800

    created login

--- src/config/routes.js
+++ src/config/routes.js
@@ -2,11 +2,13 @@ import React from 'react';
 import { Switch, Route } from 'react-router-dom';
 import Home from "../components/Home/Home";
 import Register from '../components/Auth/Register';
+import Login from '../components/Auth/Login';
 
 export default () => 
 (
   <Switch>
     <Route exact path="/" component={Home} />
     <Route path='/register' component={Register} />
+    <Route path='/login' component={Login} />
   </Switch>
 )
