757b5d940e59492a3610e212baeab21c97daeb8c
ezziagheith <ezziagheith@gmail.com>
2019-11-23 17:34:32 -0800

    Modal sucks

--- src/components/CityDetail/CityDetail.js
+++ src/components/CityDetail/CityDetail.js
@@ -15,14 +15,12 @@ const CityDetail = (props) => {
   //   )
 
   // })
-  return (
-
   if (props.cityInfo.cityName) {
     return (
       <div>
         <h2>City - {props.cityInfo.cityName} </h2>
         <h3>Country - {props.cityInfo.countryName}</h3>
-        <div><img src={`${props.cityInfo.picture}`} /></div>
+        <div><img src={`${props.cityInfo.picture}`} alt="" /></div>
   
       </div>
     )
