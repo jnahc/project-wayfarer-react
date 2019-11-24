77c4a6d2f5d3b733fcd300937467193c180bea7d
jeff-chan-sf-sei-05 <emailjeffchan@gmail.com>
2019-11-24 15:38:44 -0800

    City Post List sorted from newest to oldest

--- src/components/CityPosts/CityPosts.js
+++ src/components/CityPosts/CityPosts.js
@@ -4,13 +4,16 @@ import CityPost from '../CityPost/CityPost';
 
 const CityPosts = (props) => {
   console.log('CityPosts.js', props.posts)
-  let cityList = props.posts.map((post) => {
+  let sortedCityPostList = props.posts.sort((a,b) => a.dateCreated - b.dateCreated).reverse();
+  //CREDITS TO https://stackoverflow.com/questions/10123953/how-to-sort-an-array-by-a-date-property
+  let cityList = sortedCityPostList.map((post) => {
     return (
       <CityPost 
         key={post._id}
         postId={post._id}
         title={post.title}
         body={post.body.slice(0,50)}
+        dateCreated={post.dateCreated.toLocaleString('en-US')}
       />
     );
   })
