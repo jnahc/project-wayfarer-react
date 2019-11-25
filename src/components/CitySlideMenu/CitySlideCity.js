9139acbed6814cf4e630b54c55738ffce712e315
jacklok <jack22hk@gmail.com>
2019-11-24 22:05:36 -0800

    fixed slide for city list

--- src/components/CitySlideMenu/CitySlideCity.js
+++ src/components/CitySlideMenu/CitySlideCity.js
@@ -4,7 +4,7 @@ import './CitySlideCity.css';
 const CitySlideCity = (props) => {
     return (
         <> 
-        <a href="#">
+        <a href={`/cities/${props.urlName}`} class={window.location.pathname.split('/')[2]==`${props.urlName}`?'active':'none'}>
             <img src={`${props.picture}`} alt="city" />
              {' '}
             <span>{props.city}</span>
