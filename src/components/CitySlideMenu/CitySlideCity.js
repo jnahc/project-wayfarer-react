9613c01e75ea8ce90f7b746e71d0f9cb526dd205
ezziagheith <ezziagheith@gmail.com>
2019-11-25 15:55:36 -0800

    Fixed some errors

--- src/components/CitySlideMenu/CitySlideCity.js
+++ src/components/CitySlideMenu/CitySlideCity.js
@@ -4,7 +4,7 @@ import './CitySlideCity.css';
 const CitySlideCity = (props) => {
     return (
         <> 
-        <a href={`/cities/${props.urlName}`} className={window.location.pathname.split('/')[2]==`${props.urlName}`?'active':'none'}>
+        <a href={`/cities/${props.urlName}`} className={window.location.pathname.split('/')[2]===`${props.urlName}`?'active':'none'}>
             <img src={`${props.picture}`} alt="city" />
              {' '}
             <span className="city-name">{props.city}</span>
