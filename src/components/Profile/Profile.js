5c580ced5fe1f14ed9692d6947a55fc70e576f38
jeff-chan-sf-sei-05 <emailjeffchan@gmail.com>
2019-11-24 17:13:48 -0800

    updates styling for profile page

--- src/components/Profile/Profile.js
+++ src/components/Profile/Profile.js
@@ -4,7 +4,7 @@ const Profile = (props) => {
     console.log(props)
     if (!props.editProfile) {
         return (
-            <div style={{float:"left"}}>
+            <div style={{paddingTop:50}}>
                 <h1>{props.firstName}  {props.lastName}'s Profile</h1>
                 <img src={props.profilePhoto} alt=""/>
                 <p><strong>Email:</strong>{props.profile.email}</p>
@@ -14,33 +14,29 @@ const Profile = (props) => {
             </div>
         )
     } else {
-        return (
-            <div className="container mt-4" style={{float:"left"}}>
-                <div className="row">
-                    <div className="col-md-4 offset-md-4">
-                        <h4 className="mb-3">Edit Profile</h4>
-                        <form onSubmit={props.handleSubmit}>
-                            <div className="form-group">
-                                <label htmlFor="firstName">First Name</label>
-                                <input onChange={props.handleChange} className="form-control form-control-lg" type="text" id="firstName" name="firstName" value={props.firstName}  />
-                            </div>
-                            <div className="form-group">
-                                <label htmlFor="lastName">Last Name</label>
-                                <input onChange={props.handleChange} className="form-control form-control-lg" type="text" id="lastName" name="lastName" value={props.lastName}  />
-                            </div>
-                            <div className="form-group">
-                                <label htmlFor="currentCity">Current City</label>
-                                <input onChange={props.handleChange} className="form-control form-control-lg" type="text" id="currentCity" name="currentCity" value={props.currentCity} />
-                            </div>
-                            <div className="form-group">
-                                <label htmlFor="profilePhoto">Profile Photo URL</label>
-                                <input onChange={props.handleChange} className="form-control form-control-lg" type="text" id="profilePhoto" name="profilePhoto" value={props.profilePhoto} />
-                            </div>
-                            <button className="btn btn-primary" type="submit">Save</button>
-                        </form>
+        return ( 
+            <div style={{paddingTop:50}}>
+                <h1 className="mb-3">Edit Profile</h1>
+                <form onSubmit={props.handleSubmit}>
+                    <div className="form-group">
+                        <label htmlFor="firstName">First Name</label>
+                        <input onChange={props.handleChange} className="form-control form-control-lg" type="text" id="firstName" name="firstName" value={props.firstName}  />
                     </div>
-                </div>
-            </div> 
+                    <div className="form-group">
+                        <label htmlFor="lastName">Last Name</label>
+                        <input onChange={props.handleChange} className="form-control form-control-lg" type="text" id="lastName" name="lastName" value={props.lastName}  />
+                    </div>
+                    <div className="form-group">
+                        <label htmlFor="currentCity">Current City</label>
+                        <input onChange={props.handleChange} className="form-control form-control-lg" type="text" id="currentCity" name="currentCity" value={props.currentCity} />
+                    </div>
+                    <div className="form-group">
+                        <label htmlFor="profilePhoto">Profile Photo URL</label>
+                        <input onChange={props.handleChange} className="form-control form-control-lg" type="text" id="profilePhoto" name="profilePhoto" value={props.profilePhoto} />
+                    </div>
+                    <button className="btn btn-primary" type="submit">Save</button>
+                </form>     
+            </div>      
         )
     }
 };
